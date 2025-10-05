8d765ee735ecd6faf1534935d810aa3121c57f63
salma hesham <162762766+salmahesha@users.noreply.github.com>
2025-10-05 14:22:41 +0300

    done

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 var active = document.querySelectorAll('.links ul li a');
 var links = document.querySelector('.links');
 var bars = document.querySelector('button.bars');
+var nav = document.querySelector('nav');
 for (let i = 0; i < active.length; i++) {
     active[i].addEventListener('click',function(e) {
         for (let i = 0; i < active.length; i++){
@@ -14,5 +15,11 @@ if(bars){
     bars.addEventListener('click',function(e){
         console.log(e.currentTarget);
         links.classList.toggle('menu');
-    })
+    });
+   document.addEventListener('click', function (e) {
+    if (!nav.contains(e.target)) {
+        links.classList.remove('menu');
+    }
+});
+
 }
